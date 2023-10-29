6fa1d0d4ec8417f870edcba61497d6eaf77b1bba
Darkness-2 <justinmmm690@gmail.com>
2023-10-29 13:21:52 -0400

    Standardize environment variables with t3

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+/**
+ * Runs the environment variables checker at build time to flag and environment variables issues.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
 await import("./src/env.mjs");
 
 /** @type {import('next').NextConfig} */
