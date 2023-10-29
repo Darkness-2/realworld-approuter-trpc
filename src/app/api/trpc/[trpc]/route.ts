1070ed7eee06b07024a10f7a0ad5f326cc2aef0e
Darkness-2 <justinmmm690@gmail.com>
2023-10-29 13:30:10 -0400

    Add tRPC error message from t3

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,3 +1,4 @@
+import { env } from "$/env.mjs";
 import { appRouter } from "$/lib/server/trpc/root";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { NextRequest } from "next/server";
@@ -7,7 +8,13 @@ const handler = (req: NextRequest) =>
 		endpoint: "/api/trpc",
 		req,
 		router: appRouter,
-		createContext: () => ({})
+		createContext: () => ({}),
+		onError:
+			env.NODE_ENV === "development"
+				? ({ path, error }) => {
+						console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+				  }
+				: undefined
 	});
 
 export { handler as GET, handler as POST };
