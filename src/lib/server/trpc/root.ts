29e43a71e9260da817a1ff2b953d4ec17a521f86
Darkness-2 <justinmmm690@gmail.com>
2023-10-28 21:53:59 -0400

    Add todo procedures and cache them

--- src/lib/server/trpc/root.ts
+++ src/lib/server/trpc/root.ts
@@ -1,9 +1,30 @@
-import { createTRPCRouter, publicProcedure } from "./trpc";
+import { db } from "$/lib/server/db";
+import { todos } from "$/lib/server/db/schema";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { cache } from "react";
+import { z } from "zod";
+import { createTRPCRouter, testProcedure } from "./trpc";
+
+const generateString = cache(() => {
+	console.log("Hello ran");
+	return `Hey from the server at time: ${new Date().getTime()}`;
+});
+
+const getTodos = cache(async () => {
+	console.log("Getting todos from DB");
+	return await db.select().from(todos);
+});
 
 export const appRouter = createTRPCRouter({
-	hello: publicProcedure.query(() => {
-		return "Hey from the server";
+	hello: testProcedure.query(() => generateString()),
+	getTodos: testProcedure.query(async () => getTodos()),
+	addTodo: testProcedure.input(z.string()).mutation(async ({ input }) => {
+		await db.insert(todos).values({ content: input, done: false });
+		return true;
 	})
 });
 
 export type AppRouter = typeof appRouter;
+
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
