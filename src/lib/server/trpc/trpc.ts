2403d2c647fd0b835c02ea234a4ae287c4f9b4e8
Darkness-2 <justinmmm690@gmail.com>
2023-10-28 21:53:39 -0400

    Add test middleware

--- src/lib/server/trpc/trpc.ts
+++ src/lib/server/trpc/trpc.ts
@@ -1,6 +1,17 @@
 import { initTRPC } from "@trpc/server";
+import { cache } from "react";
 
 const t = initTRPC.create();
 
+const someMiddleware = cache(() => {
+	console.log("Middleware ran");
+});
+
+const testMiddleware = t.middleware(async ({ next }) => {
+	someMiddleware();
+	return next();
+});
+
 export const createTRPCRouter = t.router;
 export const publicProcedure = t.procedure;
+export const testProcedure = t.procedure.use(testMiddleware);
