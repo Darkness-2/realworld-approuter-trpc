dc4165d774f9ee075d48c7909ddf4ac69b913df4
Darkness-2 <justinmmm690@gmail.com>
2023-10-29 13:40:51 -0400

    Add schema to drizzle init

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,7 @@
 import { env } from "$/env.mjs";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "./schema";
 
 // Todo: Determine if this needs to be cached per request as recommended in the Neon docs
 // See: https://neon.tech/docs/serverless/serverless-driver#how-to-use-the-driver-over-http
@@ -8,4 +9,4 @@ export const pool = new Pool({
 	connectionString: env.DATABASE_URL
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema });
