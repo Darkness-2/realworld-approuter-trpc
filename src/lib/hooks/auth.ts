c7f1adc7e8c05977b4049efb85ff662318ef503e
Darkness-2 <justinmmm690@gmail.com>
2023-10-30 23:51:22 -0400

    Update comment

--- src/lib/hooks/auth.ts
+++ src/lib/hooks/auth.ts
@@ -1,7 +1,7 @@
 import { trpc } from "$/lib/trpc/client";
 
 /**
- * Hook to help get the current user.
+ * Hook to get the current user.
  * Undefined if still loading, null if no user, otherwise will have a user object.
  * @returns standard useQuery items + the user object
  */
