99440dc6878b68de7bcfe4b917de05a3da5ef8de
Darkness-2 <justinmmm690@gmail.com>
2023-10-31 17:25:57 -0400

    Remove old home page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import LogoutButton from "$/components/LogoutButton";
 import { getServerClient } from "$/lib/trpc/serverClient";
-import Link from "next/link";
 
 export default async function Home() {
 	const hello = await getServerClient().hello();
@@ -8,21 +6,6 @@ export default async function Home() {
 	return (
 		<>
 			<div>Greeting: {hello}</div>
-			<ul>
-				<li>
-					{/* Todo - convert these to Chakra links */}
-					<Link href="/auth/login">Login</Link>
-				</li>
-				<li>
-					<Link href="/auth/signup">Sign up</Link>
-				</li>
-				<li>
-					<Link href="/profile">Profile</Link>
-				</li>
-				<li>
-					<LogoutButton />
-				</li>
-			</ul>
 		</>
 	);
 }
