1697927949d6bf61cfb9043f4fc2bf3a8fd30b44
Darkness-2 <justinmmm690@gmail.com>
2023-11-03 14:55:26 -0400

    Add create article procedure

---
+++ src/server/trpc/routers/article.ts
@@ -0,0 +1,53 @@
+import { createArticleSchema } from "$/lib/schemas/article";
+import { ArticleError } from "$/lib/utils/errors";
+import { article, articlesToTags, tag } from "$/server/db/schema/article";
+import { createTRPCRouter, privateProcedure } from "$/server/trpc/trpc";
+import { TRPCError } from "@trpc/server";
+import { inArray } from "drizzle-orm";
+
+export const articleRouter = createTRPCRouter({
+	create: privateProcedure.input(createArticleSchema).mutation(async ({ input, ctx }) => {
+		// Todo: Explore doing this in a transaction
+
+		// Create article
+		const newArticleQuery = ctx.db
+			.insert(article)
+			.values({
+				authorId: ctx.user.userId,
+				title: input.title,
+				description: input.description,
+				body: input.body
+			})
+			.returning({ id: article.id });
+
+		// Convert tags to format DB expects and create if needed
+		const tagsToInsert = input.tags?.map((tag) => ({ text: tag }));
+		const newTagsQuery = tagsToInsert ? ctx.db.insert(tag).values(tagsToInsert).onConflictDoNothing() : null;
+
+		// Run queries
+		const [newArticles] = await Promise.all([newArticleQuery, newTagsQuery]);
+
+		// Return if no tags needed
+		if (!input.tags || input.tags.length === 0) {
+			return { success: true };
+		}
+
+		// Find all needed tags
+		const tags = await ctx.db.query.tag.findMany({
+			where: ({ text }) => inArray(text, input.tags ?? [])
+		});
+
+		// Attach tags and article together
+		const articleId = newArticles[0]?.id;
+
+		if (!articleId) {
+			throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", cause: new ArticleError("ARTICLE_FAILED_TO_CREATE") });
+		}
+
+		// Generate connections needed and add to DB
+		const articleTagConnections = tags.map((tag) => ({ tagId: tag.id, articleId }));
+		await ctx.db.insert(articlesToTags).values(articleTagConnections);
+
+		return { success: true };
+	})
+});
