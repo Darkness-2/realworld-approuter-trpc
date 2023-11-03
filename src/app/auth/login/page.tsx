3c975dc34e0ad500da3b1facdc0fd5ec26abcab9
Darkness-2 <justinmmm690@gmail.com>
2023-11-02 23:45:50 -0400

    Add todo comment

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,6 +2,8 @@ import AuthPage from "$/app/auth/AuthPage";
 import LoginForm from "$/app/auth/login/LoginForm";
 import { withRequireNoSession } from "$/components/hocs/withSession";
 
+// Todo: Add ?redirectTo= functionality
+
 async function LoginPage() {
 	return (
 		<AuthPage title="Login" linkHref="/auth/signup" linkText="Need an account?">
