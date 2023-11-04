891a164a41f9b6c3f8406e178c7eafa7e2843107
Darkness-2 <justinmmm690@gmail.com>
2023-11-04 14:27:22 -0400

    Fix section styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import HomePageHero from "$/components/HomePageHero";
 import HomePageTabs from "$/components/HomePageTabs";
+import Section from "$/components/ui/Section";
 import { Container } from "@chakra-ui/react";
 
 export default function Home() {
 	return (
 		<>
 			<HomePageHero />
-			<Container maxW="6xl" mt={8}>
-				<HomePageTabs />
-			</Container>
+			<Section>
+				<Container maxW="6xl">
+					<HomePageTabs />
+				</Container>
+			</Section>
 		</>
 	);
 }
--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -2,5 +2,5 @@ import { Box } from "@chakra-ui/react";
 import { type ReactNode } from "react";
 
 export default function Section({ children }: { children: ReactNode }) {
-	return <Box p={8}>{children}</Box>;
+	return <Box py={8}>{children}</Box>;
 }
