f7a9259a8aa0b76f9198438d7265325c2cb90088
Darkness-2 <justinmmm690@gmail.com>
2023-11-04 16:30:23 -0400

    Remove unneeded comment

--- src/app/article/[articleId]/page.tsx
+++ src/app/article/[articleId]/page.tsx
@@ -14,9 +14,6 @@ type ArticlePageProps = {
 
 // Todo: Add not-found boundary
 
-// Todo: Look into generateStaticParams
-// See: https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#generating-static-params
-
 export default async function ArticlePage({ params }: ArticlePageProps) {
 	const article = await getServerClient().article.getArticleById(params.articleId);
 	if (!article) notFound();
