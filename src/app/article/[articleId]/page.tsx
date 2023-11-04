ca9b4adea84176d05154115b1f3782e4f1f78713
Darkness-2 <justinmmm690@gmail.com>
2023-11-04 16:29:59 -0400

    Add generateStaticParams to article page

--- src/app/article/[articleId]/page.tsx
+++ src/app/article/[articleId]/page.tsx
@@ -2,6 +2,7 @@ import ArticleHero from "$/app/article/[articleId]/ArticleHero";
 import ArticleTags from "$/components/ArticleTags";
 import Section from "$/components/ui/Section";
 import { getServerClient } from "$/lib/trpc/serverClient";
+import { db } from "$/server/db";
 import { Stack, StackDivider, Text } from "@chakra-ui/react";
 import { notFound } from "next/navigation";
 
@@ -42,3 +43,16 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
  * Regenerate every 5 minutes.
  */
 export const revalidate = 300;
+
+/**
+ * Will generate the page at build time for the 100 most recent articles.
+ */
+export const generateStaticParams = async (): Promise<ArticlePageProps["params"][]> => {
+	const articleIds = await db.query.article.findMany({
+		columns: { id: true },
+		limit: 100,
+		orderBy: ({ createdAt }, { desc }) => desc(createdAt)
+	});
+
+	return articleIds.map(({ id }) => ({ articleId: id }));
+};
