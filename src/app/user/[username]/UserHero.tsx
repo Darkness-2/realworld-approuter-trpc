55afb65b7cb22dcbaa29d75e8e9ad82aabe678be
Darkness-2 <justinmmm690@gmail.com>
2023-11-18 13:46:36 -0500

    Adjust buttons on user page

--- src/app/user/[username]/UserHero.tsx
+++ src/app/user/[username]/UserHero.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import FollowButton from "$/components/FollowButton";
+import { useUser } from "$/lib/hooks/auth";
 import { type RouterOutputs } from "$/lib/trpc/shared";
-import { Box, Container, Stack, Text } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+import { Box, Button, Container, Skeleton, Stack, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 type UserHeroProps = {
 	user: NonNullable<RouterOutputs["article"]["getArticlesByAuthorUsername"]>["author"];
 };
 
 export default function UserHero({ user }: UserHeroProps) {
+	const { user: sessionUser, isLoading } = useUser();
+
 	return (
 		<Box bgColor="gray.300" py={8} shadow="inner">
 			<Container maxW="6xl" centerContent>
@@ -14,8 +21,16 @@ export default function UserHero({ user }: UserHeroProps) {
 					<Text as="h1" fontSize="4xl" fontWeight="semibold">
 						{user.username}
 					</Text>
-					{/* Todo: Swap out with edit profile button for own user */}
-					<FollowButton username={user.username} />
+					{/* If loading, show skeleton */}
+					{isLoading && <Skeleton h="24px" w="100px" />}
+					{/* If logged in user is viewing their own page, show button to go their settings */}
+					{sessionUser?.username === user.username && (
+						<Button as={Link} href="/settings" size="xs" variant="solid" colorScheme="gray" leftIcon={<SettingsIcon />}>
+							Go to profile settings
+						</Button>
+					)}
+					{/* If logged in user is viewing someone else's page, show follow button */}
+					{sessionUser && sessionUser.username !== user.username && <FollowButton username={user.username} />}
 				</Stack>
 			</Container>
 		</Box>
