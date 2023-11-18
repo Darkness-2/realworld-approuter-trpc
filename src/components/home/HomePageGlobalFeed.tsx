e08f8c1ac0390205e12e8e1710ead3b7b9f125b6
Darkness-2 <justinmmm690@gmail.com>
2023-11-18 13:57:30 -0500

    Throw not found page if pages are empty

--- src/components/home/HomePageGlobalFeed.tsx
+++ src/components/home/HomePageGlobalFeed.tsx
@@ -2,6 +2,7 @@ import Article from "$/components/Article";
 import PaginationButtons from "$/components/ui/PaginationButtons";
 import { getServerClient } from "$/lib/trpc/serverClient";
 import { Stack, StackDivider } from "@chakra-ui/react";
+import { notFound } from "next/navigation";
 
 // Todo: Explore wrapping this in suspense boundary or loading.js?
 
@@ -12,10 +13,16 @@ type HomePageGlobalFeedProps = {
 };
 
 export default async function HomePageGlobalFeed({ page }: HomePageGlobalFeedProps) {
+	// If going to a zero or negative page, throw not found
+	if (page < 1) notFound();
+
 	const { articles, totalCount } = await getServerClient().article.getGlobalFeed({
 		limit: DEFAULT_PAGE_SIZE,
 		offset: (page - 1) * DEFAULT_PAGE_SIZE
 	});
+
+	// If viewing a page without any articles, throw not found
+	if (articles.length === 0) notFound();
 
 	return (
 		<Stack gap={2} divider={<StackDivider />}>
