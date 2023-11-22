087afcf017e2f6ae215256baeb2f709e8f2f4a6b
jmo-laptop <justinmmm690@gmail.com>
2023-11-21 21:07:22 -0500

    Add helper to convert tags to DB format

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,5 +1,16 @@
+import { type TagInsert } from "$/server/db/schema/article";
+
 /**
  * Helper type for Next.js search params
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional
  */
 export type SearchParams = { [key: string]: string | string[] | undefined };
+
+/**
+ * Helper format to convert a string array into the tag format the DB expects
+ * @param tags string array of tags to insert or undefined
+ * @returns array of tags (can be length 0 if tags was undefined)
+ */
+export const convertTagsToDBFormat = (tags: string[] | undefined): TagInsert[] => {
+	return tags?.map((tag) => ({ text: tag })) ?? [];
+};
