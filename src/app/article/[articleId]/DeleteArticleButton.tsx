5fca150223495f2a46f0f88b8827d6651542bb0f
jmo-laptop <justinmmm690@gmail.com>
2023-11-28 14:50:55 -0500

    Add todo comment

--- src/app/article/[articleId]/DeleteArticleButton.tsx
+++ src/app/article/[articleId]/DeleteArticleButton.tsx
@@ -23,6 +23,8 @@ export default function DeleteArticleButton({ articleId }: DeleteArticleButtonPr
 				isClosable: true
 			});
 			router.push("/");
+			// Todo: I think putting this after the push is causing duplicate requests to be sent
+			// Should review that and change order of all router.refresh() calls
 			router.refresh();
 		},
 		onError: (e) => {
