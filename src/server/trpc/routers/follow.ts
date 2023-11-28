e55a5ef2ff93842bcf3d171e261b41553022782a
jmo-laptop <justinmmm690@gmail.com>
2023-11-27 20:30:14 -0500

    Add checkIfFollowing procedure

--- src/server/trpc/routers/follow.ts
+++ src/server/trpc/routers/follow.ts
@@ -3,6 +3,8 @@ import { follow } from "$/server/db/schema/follow";
 import { createTRPCRouter, privateProcedure } from "$/server/trpc/trpc";
 import { and, eq } from "drizzle-orm";
 
+// Todo: Move all DB queries and mutations into db queries folder
+
 export const followRouter = createTRPCRouter({
 	follow: privateProcedure.input(userIdSchema).mutation(async ({ ctx, input }) => {
 		await ctx.db
@@ -21,5 +23,15 @@ export const followRouter = createTRPCRouter({
 		await ctx.db.delete(follow).where(and(eq(follow.userId, ctx.user.userId), eq(follow.authorId, input)));
 
 		return true;
+	}),
+
+	checkIfFollowing: privateProcedure.input(userIdSchema).query(async ({ ctx, input }) => {
+		const f = await ctx.db.query.follow.findFirst({
+			where: ({ authorId, userId }, { and, eq }) => and(eq(authorId, input), eq(userId, ctx.user.userId))
+		});
+
+		if (f) return true;
+
+		return false;
 	})
 });
