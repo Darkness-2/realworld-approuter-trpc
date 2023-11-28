9c97c1ca1ccfdd8e9d433bbc61e4567915b38ae9
jmo-laptop <justinmmm690@gmail.com>
2023-11-27 20:30:03 -0500

    Add follow to db instance

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as articleSchema from "./schema/article";
 import * as authSchema from "./schema/auth";
+import * as followSchema from "./schema/follow";
 
 // Todo: Determine if this needs to be cached per request as recommended in the Neon docs
 // See: https://neon.tech/docs/serverless/serverless-driver#how-to-use-the-driver-over-http
@@ -12,7 +13,7 @@ export const pool = new Pool({
 });
 
 export const db = drizzle(pool, {
-	schema: { ...authSchema, ...articleSchema },
+	schema: { ...authSchema, ...articleSchema, ...followSchema },
 	logger: env.NODE_ENV === "development"
 });
 
