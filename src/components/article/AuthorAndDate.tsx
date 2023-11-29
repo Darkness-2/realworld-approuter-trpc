a613e87794d63494cbe452430d1cdf1e5c66f9d6
Darkness-2 <justinmmm690@gmail.com>
2023-11-29 16:36:37 -0500

    Fix date hydration and timezone issue (#4)
    * Add standard date formatter
    
    * Use date formatter for dates
    
    * Add back suppress hydration warning
    
    * Test putting date in a span
    
    * Add useIsClient hook
    
    * Change date rendering to be on client
    
    * Test rendering differently on client vs server
    
    * Add suppress hydration warning
    
    * Use key and suppressHydrationWarning together
    
    * Add comment explaining suppressHydrationWarning
    
    * Test to see if date itself can work as a key
    
    * Revert "Test to see if date itself can work as a key"
    
    This reverts commit 0b45250acc318bb465e7542ccc5d500e35d90151.

--- src/components/article/AuthorAndDate.tsx
+++ src/components/article/AuthorAndDate.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Link from "$/components/ui/Link";
+import { useIsClient } from "$/lib/hooks/general";
+import { dateFormatter } from "$/lib/utils/helpers";
 import { Box, Text } from "@chakra-ui/react";
 
 type AuthorAndDateProps = {
@@ -10,20 +12,24 @@ type AuthorAndDateProps = {
 };
 
 export default function AuthorAndDate({ createdAt, username, variant }: AuthorAndDateProps) {
+	const isClient = useIsClient();
+
 	const linkTextColor = variant === "light" ? "green.400" : "green.500";
 	const dateTextColor = variant === "light" ? "gray.400" : "gray.500";
+
+	const date = dateFormatter.format(createdAt);
 
 	return (
 		<Box>
 			<Link href={`/user/${username}`} textColor={linkTextColor} fontWeight="medium">
 				@{username}
 			</Link>
-			{/* Note suppressHydrationWarning below is okay as date errors are to be expected */}
+
+			{/* Note: suppressHydrationWarning is okay here as dates are expected to be different between server and client */}
+			{/* Also using key here to ensure the date is re-rendered once on client */}
 			{/* See https://nextjs.org/docs/messages/react-hydration-error#solution-3-using-suppresshydrationwarning */}
-			<Text fontSize="xs" color={dateTextColor} suppressHydrationWarning>
-				{createdAt.toLocaleDateString("en-CA", {
-					dateStyle: "long"
-				})}
+			<Text fontSize="xs" color={dateTextColor} key={isClient.toString()} suppressHydrationWarning>
+				{date}
 			</Text>
 		</Box>
 	);
