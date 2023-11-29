721ab9ead09fc77203afc507526bbf37236256e3
jmo-laptop <justinmmm690@gmail.com>
2023-11-28 20:58:09 -0500

    Style unauthorized page

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,5 +1,15 @@
-// Todo: Style this page
+import { Button, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function UnauthorizedPage() {
-	return <div>You are unauthorized to view this page.</div>;
+	return (
+		<Flex gap={2} flexDir="column" flexGrow={1} justifyContent="center" alignItems="center">
+			<Text as="h2" fontSize="2xl">
+				You are unauthorized to view this page.
+			</Text>
+			<Button as={Link} size="sm" href="/">
+				Return home
+			</Button>
+		</Flex>
+	);
 }
