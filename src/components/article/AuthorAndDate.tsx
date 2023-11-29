8905ab54fff03376a0cccf30335064e6a53b6cbe
jmo-laptop <justinmmm690@gmail.com>
2023-11-29 15:11:44 -0500

    Use date formatter for dates

--- src/components/article/AuthorAndDate.tsx
+++ src/components/article/AuthorAndDate.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "$/components/ui/Link";
+import { dateFormatter } from "$/lib/utils/helpers";
 import { Box, Text } from "@chakra-ui/react";
 
 type AuthorAndDateProps = {
@@ -13,17 +14,15 @@ export default function AuthorAndDate({ createdAt, username, variant }: AuthorAn
 	const linkTextColor = variant === "light" ? "green.400" : "green.500";
 	const dateTextColor = variant === "light" ? "gray.400" : "gray.500";
 
+	const date = dateFormatter.format(createdAt);
+
 	return (
 		<Box>
 			<Link href={`/user/${username}`} textColor={linkTextColor} fontWeight="medium">
 				@{username}
 			</Link>
-			{/* Note suppressHydrationWarning below is okay as date errors are to be expected */}
-			{/* See https://nextjs.org/docs/messages/react-hydration-error#solution-3-using-suppresshydrationwarning */}
-			<Text fontSize="xs" color={dateTextColor} suppressHydrationWarning>
-				{createdAt.toLocaleDateString("en-CA", {
-					dateStyle: "long"
-				})}
+			<Text fontSize="xs" color={dateTextColor}>
+				{date}
 			</Text>
 		</Box>
 	);
