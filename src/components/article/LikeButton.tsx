3d0030106097fd2c53ac254a2f489e5dc3ca746d
jmo-laptop <justinmmm690@gmail.com>
2023-11-29 14:03:18 -0500

    Add like / unlike functionality to like button

--- src/components/article/LikeButton.tsx
+++ src/components/article/LikeButton.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useLikedArticles } from "$/lib/hooks/article";
+import { trpc } from "$/lib/trpc/client";
 import { StarIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
-
-// Todo: Make button like the article
+import { Button, useToast } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 
 type LikeButtonProps = {
 	likes: number;
@@ -12,21 +12,70 @@ type LikeButtonProps = {
 };
 
 export default function LikeButton({ likes, articleId }: LikeButtonProps) {
+	const router = useRouter();
+	const utils = trpc.useUtils();
+	const toast = useToast();
 	const { likedArticles, isLoading } = useLikedArticles();
+
+	const like = trpc.article.likeArticle.useMutation({
+		onError: (e) => {
+			// Something unexpected happened
+			toast({
+				title: "Something went wrong",
+				description: "Unable to like the article",
+				status: "error",
+				isClosable: true
+			});
+
+			console.error(e);
+		},
+		onSettled: () => {
+			// Invalidate liked articles query and refresh the router
+			utils.article.getLikedArticles.invalidate();
+			router.refresh();
+		}
+	});
+
+	const unlike = trpc.article.unlikeArticle.useMutation({
+		onError: (e) => {
+			// Something unexpected happened
+			toast({
+				title: "Something went wrong",
+				description: "Unable to unlike the article",
+				status: "error",
+				isClosable: true
+			});
+
+			console.error(e);
+		},
+		onSettled: () => {
+			// Invalidate liked articles query and refresh the router
+			utils.article.getLikedArticles.invalidate();
+			router.refresh();
+		}
+	});
 
 	// Determine if the user likes this article
 	const isLiked = likedArticles && likedArticles.some((a) => a.articleId === articleId);
 
+	// Todo: Look into optimistic update for this
+
+	const handleClick = () => {
+		if (isLiked) unlike.mutate(articleId);
+		if (!isLiked) like.mutate(articleId);
+	};
+
 	return (
 		<Button
-			isLoading={isLoading}
+			isLoading={isLoading || like.isLoading || unlike.isLoading}
 			size="xs"
 			variant="solid"
 			// Show as gray while loading or if user has liked
 			colorScheme={isLiked || isLoading ? "gray" : "green"}
 			leftIcon={<StarIcon />}
+			onClick={handleClick}
 		>
-			Like Article ({likes})
+			{isLiked ? "Unlike" : "Like"} article ({likes})
 		</Button>
 	);
 }
