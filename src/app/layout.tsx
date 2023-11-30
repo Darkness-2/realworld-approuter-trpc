51a83cb9a540b6829656e4e1390bee526eb65d45
jmo-laptop <justinmmm690@gmail.com>
2023-11-29 19:44:30 -0500

    Move Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "$/components/Navbar";
+import Navbar from "$/components/ui/Navbar";
 import "$/styles/globals.css";
 import { Flex } from "@chakra-ui/react";
 import type { Metadata } from "next";
