327bf513340af6cfe97c1b69dbf539b409018d96
Darkness-2 <justinmmm690@gmail.com>
2023-12-02 16:28:10 -0500

    Refactor page to use searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import HomePage from "$/components/home/HomePage";
+import HomePageGlobalFeed from "$/components/home/HomePageGlobalFeed";
+import HomePageHero from "$/components/home/HomePageHero";
+import Section from "$/components/ui/Section";
+import { type SearchParams } from "$/lib/utils/helpers";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 
-export default function Home() {
-	return <HomePage page={1} />;
+type HomePageProps = {
+	searchParams: SearchParams;
+};
+
+export default function HomePage({ searchParams }: HomePageProps) {
+	const pageString = "page" in searchParams && typeof searchParams.page === "string" ? searchParams.page : "1";
+	const page = parseInt(pageString);
+
+	return (
+		<>
+			<HomePageHero />
+			<Section>
+				<Tabs>
+					<TabList>
+						<Tab>Global feed</Tab>
+						<Tab>Your feed</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel px={0}>
+							<HomePageGlobalFeed page={page} />
+						</TabPanel>
+						<TabPanel px={0}>Todo: Put in user&apos;s feed</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</Section>
+		</>
+	);
 }
 
 export const revalidate = 300;
