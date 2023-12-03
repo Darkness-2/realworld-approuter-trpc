8d9ac6a9159811054547deb2e470e389d2ff5a3d
Darkness-2 <justinmmm690@gmail.com>
2023-12-02 20:12:52 -0500

    Add limit cursor schema

--- src/lib/schemas/helpers.ts
+++ src/lib/schemas/helpers.ts
@@ -1,6 +1,17 @@
 import { z } from "zod";
 
+/**
+ * Schema for standard limit-offset pagination.
+ */
 export const limitOffsetSchema = z.object({
 	limit: z.number().min(1).max(100).default(10),
 	offset: z.number().min(0).default(0)
+});
+
+/**
+ * Schema for cursor pagination, using createdAt timestamps.
+ */
+export const limitCreatedAtCursorSchema = z.object({
+	limit: z.number().min(1).max(100).default(10),
+	cursor: z.date().optional()
 });
