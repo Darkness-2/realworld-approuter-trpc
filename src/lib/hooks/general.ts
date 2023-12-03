d0d4c79fa3dd4e9a378223b911427137e754c19e
jmo-laptop <justinmmm690@gmail.com>
2023-12-03 18:42:09 -0500

    Add comment

--- src/lib/hooks/general.ts
+++ src/lib/hooks/general.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Simple hook that returns a boolean of whether you are on client or not.
+ */
 export const useIsClient = () => {
 	const [isClient, setIsClient] = useState(false);
 
