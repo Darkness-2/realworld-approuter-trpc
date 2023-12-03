58ccc5278835825e5cca0531bcd7fbf65097d3a4
Darkness-2 <justinmmm690@gmail.com>
2023-12-03 14:09:59 -0500

    Memoize data transformation

--- src/app/user/[username]/InfiniteUserArticleScroll.tsx
+++ src/app/user/[username]/InfiniteUserArticleScroll.tsx
@@ -7,7 +7,7 @@ import { type RouterOutputs } from "$/lib/trpc/shared";
 import { Button, Flex, Stack, StackDivider } from "@chakra-ui/react";
 import { useQueryClient } from "@tanstack/react-query";
 import { getQueryKey } from "@trpc/react-query";
-import { type ComponentProps } from "react";
+import { useMemo, type ComponentProps } from "react";
 
 type InfiniteArticleScrollProps = {
 	username: string;
@@ -25,50 +25,51 @@ export default function InfiniteUserArticleScroll({
 	const utils = trpc.useUtils();
 	const queryClient = useQueryClient();
 
-	const queryKey = getQueryKey(trpc.article.getArticlesByAuthorUsername, { username, limit: pageSize }, "infinite");
+	const queryInputs = { username, limit: pageSize };
+
+	const queryKey = getQueryKey(trpc.article.getArticlesByAuthorUsername, queryInputs, "infinite");
 	const state = queryClient.getQueryState(queryKey);
 
 	// Set the initial data if it is fresher than existing data or if no data exists
 	if (!state || state.dataUpdatedAt < initialDataTimestamp) {
 		utils.article.getArticlesByAuthorUsername.setInfiniteData(
-			{ username, limit: pageSize },
+			queryInputs,
 			{ pages: [initialData], pageParams: [undefined] },
 			{ updatedAt: initialDataTimestamp }
 		);
 	}
 
 	const { data, hasNextPage, fetchNextPage, isFetching, isLoading } =
-		trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(
-			{ username, limit: pageSize },
-			{
-				getNextPageParam: (lastPage) => {
-					// Shouldn't theoretically happen as at this point we know the username exists
-					if (!lastPage) return undefined;
+		trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(queryInputs, {
+			getNextPageParam: (lastPage) => {
+				// Shouldn't theoretically happen as at this point we know the username exists
+				if (!lastPage) return undefined;
 
-					// If there are no more pages, return undefined
-					if (!lastPage.hasMore) return undefined;
+				// If there are no more pages, return undefined
+				if (!lastPage.hasMore) return undefined;
 
-					// Else, return the last createdAt timestamp
-					const lastArticle = lastPage.articles.slice(-1);
-					return lastArticle?.[0]?.createdAt;
-				},
-				// Setting stale time to the same as the page revalidate time
-				// Mutations changing articles should call article.invalidate anyway
-				staleTime: 5 * 60 * 1000
-			}
-		);
+				// Else, return the last createdAt timestamp
+				const lastArticle = lastPage.articles.slice(-1);
+				return lastArticle?.[0]?.createdAt;
+			},
+			// Setting stale time to the same as the page revalidate time
+			// Mutations changing articles should call article.invalidate anyway
+			staleTime: 5 * 60 * 1000
+		});
 
 	// Merge articles from the pages together and format for the ArticleList component
-	// Todo: Does tanstack not cover this automatically in select?
-	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] =
-		data?.pages.flatMap((page) => {
-			// Theoretically shouldn't be null as at this point we know the username exists
-			if (!page) return [];
+	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] = useMemo(
+		() =>
+			data?.pages.flatMap((page) => {
+				// Theoretically shouldn't be null as at this point we know the username exists
+				if (!page) return [];
 
-			// Add author information back into the articles
-			const articles = page.articles.map((article) => ({ ...article, author: { username } }));
-			return [...articles];
-		}) ?? [];
+				// Add author information back into the articles
+				const articles = page.articles.map((article) => ({ ...article, author: { username } }));
+				return [...articles];
+			}) ?? [],
+		[data, username]
+	);
 
 	return (
 		<Stack gap={2} divider={<StackDivider />}>
