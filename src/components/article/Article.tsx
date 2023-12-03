4728d4d426656f77542e98ecad4f60069f7f5239
Darkness-2 <justinmmm690@gmail.com>
2023-12-03 13:55:00 -0500

    Add loading state to articles

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -3,51 +3,80 @@ import AuthorAndDate from "$/components/article/AuthorAndDate";
 import LikesTag from "$/components/like/LikesTag";
 import Link from "$/components/ui/Link";
 import { type RouterOutputs } from "$/lib/trpc/shared";
-import { Flex, Stack, Text } from "@chakra-ui/react";
+import { Flex, Skeleton, Stack, Text } from "@chakra-ui/react";
 
-type ArticleProps = {
-	article: RouterOutputs["article"]["getGlobalFeed"]["articles"][number];
+type Article = RouterOutputs["article"]["getGlobalFeed"]["articles"][number];
+
+type ArticleProps =
+	| {
+			article: Article;
+			isLoading?: false;
+	  }
+	| {
+			article?: undefined;
+			isLoading: true;
+	  };
+
+// Used if Article is in loading state
+const mockData: Article = {
+	likesCount: 0,
+	articlesToTags: [{ tag: { id: "abc", text: "abc" } }],
+	author: {
+		username: "abc"
+	},
+	authorId: "abc",
+	createdAt: new Date(),
+	description: "abc",
+	id: "abc",
+	title: "abc"
 };
 
-export default function Article({ article }: ArticleProps) {
+export default function Article({ article = mockData, isLoading = false }: ArticleProps) {
 	const articleHref = `/article/${article.id}`;
 	const articleTags = article.articlesToTags.map(({ tag }) => tag);
 
 	return (
 		<Stack>
 			{/* Top row */}
-			<Flex
-				gap={2}
-				// flexDirection={{ base: "column", sm: "row" }}
-				justifyContent="space-between"
-				alignItems="center"
-			>
-				<AuthorAndDate variant="dark" createdAt={article.createdAt} username={article.author.username} />
-				<LikesTag likes={article.likesCount} />
+
+			<Flex gap={2} justifyContent="space-between" alignItems="center">
+				<Skeleton isLoaded={!isLoading}>
+					<AuthorAndDate variant="dark" createdAt={article.createdAt} username={article.author.username} />
+				</Skeleton>
+				<Skeleton isLoaded={!isLoading}>
+					<LikesTag likes={article.likesCount} />
+				</Skeleton>
 			</Flex>
 
 			{/* Main text content */}
 			<Link href={articleHref} _hover={{ textDecoration: "none" }}>
 				<Stack>
-					<Text fontSize="2xl" fontWeight="semibold">
-						{article.title}
-					</Text>
-					<Text color="gray.500">{article.description}</Text>
+					{/* Todo: Replace other skeletons with this approach */}
+					<Skeleton isLoaded={!isLoading}>
+						<Text fontSize="2xl" fontWeight="semibold">
+							{article.title}
+						</Text>
+					</Skeleton>
+					<Skeleton isLoaded={!isLoading}>
+						<Text color="gray.500">{article.description}</Text>
+					</Skeleton>
 				</Stack>
 			</Link>
 
 			{/* Bottom row */}
-			<Flex
-				gap={2}
-				flexDirection={{ base: "column", sm: "row" }}
-				justifyContent="space-between"
-				alignItems={{ base: "normal", sm: "center" }}
-			>
-				<Link href={articleHref} fontSize="xs" color="gray.500">
-					Read more...
-				</Link>
-				<ArticleTags tags={articleTags} />
-			</Flex>
+			<Skeleton isLoaded={!isLoading}>
+				<Flex
+					gap={2}
+					flexDirection={{ base: "column", sm: "row" }}
+					justifyContent="space-between"
+					alignItems={{ base: "normal", sm: "center" }}
+				>
+					<Link href={articleHref} fontSize="xs" color="gray.500">
+						Read more...
+					</Link>
+					<ArticleTags tags={articleTags} />
+				</Flex>
+			</Skeleton>
 		</Stack>
 	);
 }
