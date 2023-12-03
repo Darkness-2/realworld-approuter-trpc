fc3bbe111808977170bebd6e92289a36326dc9d9
Darkness-2 <justinmmm690@gmail.com>
2023-12-02 20:52:22 -0500

    Enhance has next page logic

--- src/app/user/[username]/InfiniteUserArticleScroll.tsx
+++ src/app/user/[username]/InfiniteUserArticleScroll.tsx
@@ -14,40 +14,47 @@ type InfiniteArticleScrollProps = {
 };
 
 export default function InfiniteUserArticleScroll({ username, initialData }: InfiniteArticleScrollProps) {
-	const infiniteQuery = trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(
+	const { data, hasNextPage, fetchNextPage } = trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(
 		{ username, limit: DEFAULT_PAGE_SIZE },
 		{
 			initialData: {
 				pages: [initialData],
 				pageParams: []
 			},
+			// Todo: Probably want to configure this to not refetch every page again when revisiting the page
 			getNextPageParam: (lastPage) => {
-				const lastArticle = lastPage?.articles.slice(-1);
+				// Shouldn't theoretically happen as at this point we know the username exists
+				if (!lastPage) return undefined;
+
+				// If last page already wasn't full, no more pages exist
+				if (lastPage.articles.length < DEFAULT_PAGE_SIZE) return undefined;
+
+				// Todo: Test this the user has a number of articles divisible by 10
+				// Probably better to return a hasMore from the tRPC procedure
+
+				const lastArticle = lastPage.articles.slice(-1);
 				return lastArticle?.[0]?.createdAt;
 			}
 		}
 	);
 
-	console.log(infiniteQuery.data);
-
 	// Merge articles from the pages together and format for the ArticleList component
 	// Todo: Does tanstack not cover this automatically?
-	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] | undefined = infiniteQuery.data?.pages.flatMap(
-		(page) => {
-			if (!page) return [];
+	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] | undefined = data?.pages.flatMap((page) => {
+		// Returns theoretically shouldn't be null as at this point we know the username exists
+		if (!page) return [];
 
-			// Add author information back into the articles
-			const articles = page.articles.map((article) => ({ ...article, author: { username } }));
-			return [...articles];
-		}
-	);
+		// Add author information back into the articles
+		const articles = page.articles.map((article) => ({ ...article, author: { username } }));
+		return [...articles];
+	});
 
 	return (
 		<>
 			{/* Todo: Add loading state */}
 			{mergedArticles && <ArticleList articles={mergedArticles} />}
 			{/* Todo: Loading state for this button */}
-			<Button onClick={() => infiniteQuery.fetchNextPage()}>Fetch more</Button>
+			{hasNextPage && <Button onClick={() => fetchNextPage()}>Fetch more</Button>}
 		</>
 	);
 }
