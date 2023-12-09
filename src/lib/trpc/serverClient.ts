1c1c1684aead5ebf133c32ebb642f99422185304
Darkness-2 <justinmmm690@gmail.com>
2023-12-09 14:05:27 -0500

    Replace all uses of old server client with new helpers

--- src/lib/trpc/serverClient.ts
+++ src/lib/trpc/serverClient.ts
@@ -5,33 +5,16 @@ import { cache } from "react";
 import superjson from "superjson";
 
 /**
- * Factory to create server-side callers for tRPC.
- * Needed as this ensures every request will get a new, unique caller.
+ * Cached factory to get the tRPC server-side helpers.
+ * Needed as this ensures every request gets a new, unique helper.
  *
- * NOTE: Intentionally not using dynamic functions like headers and cookies,
- * in order to ensure that pages using this are still cachable.
+ * NOTE: Intentionally avoiding the use of dynamic functions like headers and cookies,
+ * in context creation in order to ensure that most pages are still cachable.
  *
- * @returns a server-side caller for tRPC
+ * @returns the tRPC server-side helpers
+ * @see https://trpc.io/docs/client/nextjs/server-side-helpers
  */
-export const createServerClient = () => {
-	const context = createTRPCContext({ source: "server" });
-	return appRouter.createCaller(context);
-};
-
-/**
- * React cache wrapper around the factory to avoid the overhead of generating a new caller if
- * it is used in multiple places in a single request (for example in two separate server components).
- *
- * Should avoid the context being created multiple times.
- *
- * Todo: Consider if this needs to be wrapped in cache given I moved most context stuff into middleware.
- */
-export const getServerClient = cache(createServerClient);
-
-/**
- * Todo: Document this and replace all usage of the above functions.
- */
-export const getServerSideHelpers = cache(() =>
+export const getServerTRPCClient = cache(() =>
 	createServerSideHelpers({
 		router: appRouter,
 		ctx: createTRPCContext({ source: "server-helpers" }),
