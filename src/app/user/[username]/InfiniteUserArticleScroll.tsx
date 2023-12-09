ab9c495f08b9392b5741bc30ce96b35f2ca560e9
Darkness-2 <justinmmm690@gmail.com>
2023-12-09 13:51:17 -0500

    Remove useMemo

--- src/app/user/[username]/InfiniteUserArticleScroll.tsx
+++ src/app/user/[username]/InfiniteUserArticleScroll.tsx
@@ -4,7 +4,7 @@ import Article from "$/components/article/Article";
 import ArticleList from "$/components/article/ArticleList";
 import { trpc } from "$/lib/trpc/client";
 import { Button, Flex, Stack, StackDivider } from "@chakra-ui/react";
-import { useMemo, type ComponentProps } from "react";
+import { type ComponentProps } from "react";
 
 type InfiniteArticleScrollProps = {
 	username: string;
@@ -12,39 +12,38 @@ type InfiniteArticleScrollProps = {
 };
 
 export default function InfiniteUserArticleScroll({ username, pageSize }: InfiniteArticleScrollProps) {
-	const queryInputs = { username, limit: pageSize };
-
 	const { data, hasNextPage, fetchNextPage, isFetching, isLoading } =
-		trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(queryInputs, {
-			getNextPageParam: (lastPage) => {
-				// Shouldn't theoretically happen as at this point we know the username exists
-				if (!lastPage) return undefined;
+		trpc.article.getArticlesByAuthorUsername.useInfiniteQuery(
+			{ username, limit: pageSize },
+			{
+				getNextPageParam: (lastPage) => {
+					// Shouldn't theoretically happen as at this point we know the username exists
+					if (!lastPage) return undefined;
 
-				// If there are no more pages, return undefined
-				if (!lastPage.hasMore) return undefined;
+					// If there are no more pages, return undefined
+					if (!lastPage.hasMore) return undefined;
 
-				// Else, return the last createdAt timestamp
-				const lastArticle = lastPage.articles.slice(-1);
-				return lastArticle?.[0]?.createdAt;
-			},
-			// Setting stale time to the same as the page revalidate time
-			// Mutations changing articles should call article.invalidate anyway
-			staleTime: 5 * 60 * 1000
-		});
+					// Else, return the last createdAt timestamp
+					const lastArticle = lastPage.articles.slice(-1);
+					return lastArticle?.[0]?.createdAt;
+				},
+				// Setting stale time to the same as the page revalidate time
+				// Mutations changing articles should call article.invalidate anyway
+				staleTime: 5 * 60 * 1000
+			}
+		);
 
 	// Merge articles from the pages together and format for the ArticleList component
-	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] = useMemo(() => {
-		return (
-			data?.pages.flatMap((page) => {
-				// Theoretically shouldn't be null as at this point we know the username exists
-				if (!page) return [];
+	// Todo: Can react query select do this?
+	const mergedArticles: ComponentProps<typeof ArticleList>["articles"] =
+		data?.pages.flatMap((page) => {
+			// Theoretically shouldn't be null as at this point we know the username exists
+			if (!page) return [];
 
-				// Add author information back into the articles
-				const articles = page.articles.map((article) => ({ ...article, author: { username } }));
-				return [...articles];
-			}) ?? []
-		);
-	}, [data, username]);
+			// Add author information back into the articles
+			const articles = page.articles.map((article) => ({ ...article, author: { username } }));
+			return [...articles];
+		}) ?? [];
 
 	return (
 		<Stack gap={2} divider={<StackDivider />}>
