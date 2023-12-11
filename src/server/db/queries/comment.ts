87d15cd45388c4ca7e275dacfd8909a30622c296
jmo-laptop <justinmmm690@gmail.com>
2023-12-10 20:59:39 -0500

    Add getArticleComments procedure

---
+++ src/server/db/queries/comment.ts
@@ -0,0 +1,31 @@
+import { generateFutureDate } from "$/lib/utils/helpers";
+import { type DB } from "$/server/db";
+import { cache } from "react";
+
+/** Queries */
+
+/**
+ * Cached database call to get an article's comments.
+ * Includes comment author username.
+ *
+ * @param db instance of the DB
+ * @param articleId id of the article
+ * @param limit how many items to get
+ * @param cursor createdAt timestamp to get comments older than
+ * @returns array of comments for the article
+ */
+export const getArticleCommentsQuery = cache(async (db: DB, articleId: string, limit: number, cursor?: Date) => {
+	// If date is undefined, generate a random date far into the future
+	const date = cursor ?? generateFutureDate();
+
+	return await db.query.comment.findMany({
+		where: ({ articleId: aid, createdAt }, { and, eq, lt }) => and(eq(aid, articleId), lt(createdAt, date)),
+		orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+		limit,
+		with: {
+			author: { columns: { username: true } }
+		}
+	});
+});
+
+/** Mutations */
--- src/server/trpc/routers/comment.ts
+++ src/server/trpc/routers/comment.ts
@@ -1,6 +1,28 @@
-import { createTRPCRouter } from "$/server/trpc/trpc";
+import { articleIdSchema } from "$/lib/schemas/article";
+import { limitDateCursorSchema } from "$/lib/schemas/helpers";
+import { getArticleCommentsQuery } from "$/server/db/queries/comment";
+import { createTRPCRouter, publicProcedure } from "$/server/trpc/trpc";
 
-const queries = {};
+const queries = {
+	getArticleComments: publicProcedure
+		.input(
+			limitDateCursorSchema.extend({
+				articleId: articleIdSchema
+			})
+		)
+		.query(async ({ ctx, input }) => {
+			// Grab one more than limit so we can tell if there are more comments
+			const comments = await getArticleCommentsQuery(ctx.db, input.articleId, input.limit + 1, input.cursor);
+
+			// Determine if we have a next page
+			const hasMore = comments.length > input.limit;
+
+			// Pop last one as we grabbed more than needed
+			if (hasMore) comments.pop();
+
+			return { comments, hasMore };
+		})
+};
 
 const mutations = {};
 
