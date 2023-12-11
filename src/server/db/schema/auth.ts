e2b10f7c488a81f07adb9e60c60c52c373de99ef
jmo-laptop <justinmmm690@gmail.com>
2023-12-10 19:40:03 -0500

    Add comment schema

--- src/server/db/schema/auth.ts
+++ src/server/db/schema/auth.ts
@@ -3,6 +3,7 @@ import { relations } from "drizzle-orm";
 import { bigint, varchar } from "drizzle-orm/pg-core";
 import { pgTable } from "../root";
 import { article } from "./article";
+import { comment } from "./comment";
 import { like } from "./like";
 
 /**
@@ -23,6 +24,7 @@ export const userRelations = relations(user, ({ many }) => ({
 	sessions: many(session),
 	keys: many(key),
 	articles: many(article),
+	comments: many(comment),
 	likes: many(like),
 	follows: many(follow, { relationName: "user" }),
 	followers: many(follow, { relationName: "author" })
