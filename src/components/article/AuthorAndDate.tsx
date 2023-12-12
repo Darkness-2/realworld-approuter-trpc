97253d747c75dc812b3c55dc09eeb0acc911c397
jmo-laptop <justinmmm690@gmail.com>
2023-12-11 20:50:14 -0500

    Create comment and comment list components

--- src/components/article/AuthorAndDate.tsx
+++ src/components/article/AuthorAndDate.tsx
@@ -1,24 +1,29 @@
 import DateText from "$/components/ui/DateText";
 import Link from "$/components/ui/Link";
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 type AuthorAndDateProps = {
 	createdAt: Date;
 	username: string;
 	variant: "light" | "dark";
+	asRow?: boolean;
 };
 
-export default function AuthorAndDate({ createdAt, username, variant }: AuthorAndDateProps) {
+export default function AuthorAndDate({ createdAt, username, variant, asRow = false }: AuthorAndDateProps) {
 	const linkTextColor = variant === "light" ? "green.400" : "green.500";
 	const dateTextColor = variant === "light" ? "gray.400" : "gray.500";
 
 	return (
-		<Box>
+		<Flex
+			flexDirection={asRow ? "row" : "column"}
+			alignItems={asRow ? "center" : undefined}
+			gap={asRow ? 2 : undefined}
+		>
 			<Link href={`/user/${username}`} textColor={linkTextColor} fontWeight="medium">
 				@{username}
 			</Link>
 
 			<DateText date={createdAt} fontSize="xs" color={dateTextColor} />
-		</Box>
+		</Flex>
 	);
 }
---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,46 @@
+import AuthorAndDate from "$/components/article/AuthorAndDate";
+import { type RouterOutputs } from "$/lib/trpc/shared";
+import { Card, CardBody, CardFooter, Skeleton, Text } from "@chakra-ui/react";
+
+type Comment = RouterOutputs["comment"]["getArticleComments"]["comments"][number];
+
+type CommentProps =
+	| {
+			comment: Comment;
+			isLoading?: false;
+	  }
+	| {
+			comment?: undefined;
+			isLoading: true;
+	  };
+
+// Used if Comment is in loading state
+const mockData: Comment = {
+	id: "abc",
+	articleId: "abc",
+	author: {
+		username: "abc"
+	},
+	authorId: "abc",
+	body: "abc",
+	createdAt: new Date()
+};
+
+export default function Comment({ comment = mockData, isLoading = false }: CommentProps) {
+	const { author, body, createdAt } = comment;
+
+	return (
+		<Card border="1px" borderColor="gray.200" boxShadow="none">
+			<CardBody py={4}>
+				<Skeleton isLoaded={!isLoading}>
+					<Text>{body}</Text>
+				</Skeleton>
+			</CardBody>
+			<CardFooter bg="gray.100" py={3}>
+				<Skeleton isLoaded={!isLoading}>
+					<AuthorAndDate createdAt={createdAt} username={author.username} variant="dark" asRow />
+				</Skeleton>
+			</CardFooter>
+		</Card>
+	);
+}
