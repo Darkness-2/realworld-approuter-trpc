c0d34b8f3a864b77b7e2076f810d7c86af9ca17c
jmo-laptop <justinmmm690@gmail.com>
2023-12-12 16:43:51 -0500

    Change comment list formatting

--- src/components/comment/CommentList.tsx
+++ src/components/comment/CommentList.tsx
@@ -1,5 +1,5 @@
 import Comment from "$/components/comment/Comment";
-import { Flex, Stack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import { type ComponentProps } from "react";
 
 type CommentListProps = {
@@ -8,12 +8,10 @@ type CommentListProps = {
 
 export default function CommentList({ comments }: CommentListProps) {
 	return (
-		<Flex justifyContent="center">
-			<Stack gap={2} w="full" maxW="xl">
-				{comments.map((comment) => (
-					<Comment key={comment.id} comment={comment} />
-				))}
-			</Stack>
-		</Flex>
+		<Stack gap={2}>
+			{comments.map((comment) => (
+				<Comment key={comment.id} comment={comment} />
+			))}
+		</Stack>
 	);
 }
