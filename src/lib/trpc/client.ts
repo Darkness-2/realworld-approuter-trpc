f9c1f9252cb15023f2597d42290907be38f5d36c
Darkness-2 <justinmmm690@gmail.com>
2023-12-21 17:39:44 -0500

    Add ability to opt out of tRPC batching

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,11 +1,16 @@
 import { getTRPCUrl } from "$/lib/trpc/shared";
 import { type AppRouter } from "$/server/trpc/root";
 import { QueryClient } from "@tanstack/react-query";
-import { httpBatchLink, loggerLink } from "@trpc/client";
+import { httpBatchLink, httpLink, loggerLink, splitLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import superjson from "superjson";
 
 export const trpc = createTRPCReact<AppRouter>({});
+
+const generateTRPCHeaders = () => ({
+	// Todo: Is it even possible to trigger this client server-side?
+	"x-trpc-source": typeof window !== "undefined" ? "client" : "server-component"
+});
 
 export const createTRPCClient = () =>
 	trpc.createClient({
@@ -16,12 +21,18 @@ export const createTRPCClient = () =>
 				enabled: (opts) =>
 					process.env.NODE_ENV === "development" || (opts.direction === "down" && opts.result instanceof Error)
 			}),
-			httpBatchLink({
-				url: getTRPCUrl(),
-				maxURLLength: 2083,
-				headers: () => ({
-					// Todo: Is it even possible to trigger this client server-side?
-					"x-trpc-source": typeof window !== "undefined" ? "client" : "server-component"
+
+			// Use batching by default, but provide opt-out via context.skipBatch
+			splitLink({
+				condition: (op) => op.context.skipBatch === true,
+				true: httpLink({
+					url: getTRPCUrl(),
+					headers: generateTRPCHeaders
+				}),
+				false: httpBatchLink({
+					url: getTRPCUrl(),
+					maxURLLength: 2083,
+					headers: generateTRPCHeaders
 				})
 			})
 		]
